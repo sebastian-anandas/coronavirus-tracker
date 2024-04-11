fe00f2b1077063c77375c9bb9e43c999968fcfba
sebastian-anandas <sebastian.anandas@gmail.com>
2024-04-12 00:46:47 +0530

    Initial Commit

---
+++ src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,15 @@
+package io.javabrains.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
